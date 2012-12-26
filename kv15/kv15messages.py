6efe649414cd3a5cc732f2373cad94000bc159fb
Stefan de Konink <stefan@konink.de>
2012-12-26 14:39:06 +0100

    Saves all the messages upon success

--- kv15/kv15messages.py
+++ kv15/kv15messages.py
@@ -1,20 +1,27 @@
 from io.push import Push
+from kv15.database import connect
 
 class KV15messages:
-	def __init__(self, stopmessages = None):
-		if stopmessages is None:
-			self.stopmessages = []
-		else:
-			self.stopmessages = stopmessages
-	
-	def __str__(self):
-		xml = """	<KV15messages>\n"""
-		for stopmessage in self.stopmessages:
-			xml += str(stopmessage)
-		xml += """	</KV15messages>"""
+    def __init__(self, stopmessages = None):
+        if stopmessages is None:
+            self.stopmessages = []
+        else:
+            self.stopmessages = stopmessages
 
-		return xml
-	
-	def push(self, remote, path):
-		return Push(dossiername='KV15messages', content = str(self), namespace='http://bison.connekt.nl/tmi8/kv15/msg').push(remote, path)
+    def __str__(self):
+        xml = """       <KV15messages>\n"""
+        for stopmessage in self.stopmessages:
+            xml += str(stopmessage)
+        xml += """      </KV15messages>"""
 
+        return xml
+
+    def push(self, remote, path):
+        return Push(dossiername='KV15messages', content = self, namespace='http://bison.connekt.nl/tmi8/kv15/msg').push(remote, path)
+
+    def save(self, cur = None):
+        if cur is None:
+            cur = connect()
+
+        for stopmessage in self.stopmessages:
+            stopmessage.save(cur)
