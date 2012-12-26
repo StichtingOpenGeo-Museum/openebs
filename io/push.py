69984e83060cbfd44e6da83d0afea1429ef74d4e
Stefan de Konink <stefan@konink.de>
2012-12-26 14:21:33 +0100

    Localize timezones, implement response handling (save)

--- io/push.py
+++ io/push.py
@@ -1,47 +1,50 @@
 from datetime import datetime
 from httplib import HTTPConnection
-from settings.const import debug, send
+from settings.const import debug, send, store
+
+import pytz
 
 class Push:
-	def __init__(self, subscriberid = 'openOV', dossiername = None, content = None, namespace = None):
-		self.subscriberid = subscriberid
-		self.timestamp = datetime.now()
-		
-		self.dossiername = dossiername
-		self.content = content
-		self.namespace = namespace
-	
-	def __str__(self):
-		data = {'namespace': self.namespace,
-		        'subscriberid': self.subscriberid,
-			'dossiername': self.dossiername,
-		        'timestamp': self.timestamp.replace(microsecond=0).isoformat(),
-			'content': self.content }
+    def __init__(self, subscriberid = 'openOV', dossiername = None, content = None, namespace = None):
+        self.subscriberid = subscriberid
+        self.timestamp = datetime.now()
 
-		xml = """<VV_TM_PUSH xmlns="%(namespace)s">
-	<SubscriberID>%(subscriberid)s</SubscriberID>
-	<Version>8.1.0.1</Version>
-	<DossierName>%(dossiername)s</DossierName>
-	<Timestamp>%(timestamp)s</Timestamp>
+        self.dossiername = dossiername
+        self.content = content
+        self.namespace = namespace
+
+    def __str__(self):
+        data = {'namespace': self.namespace,
+                'subscriberid': self.subscriberid,
+                'dossiername': self.dossiername,
+                'timestamp': pytz.timezone('Europe/Amsterdam').localize(self.timestamp.replace(microsecond=0)).isoformat(),
+                'content': str(self.content) }
+
+        xml = """<VV_TM_PUSH xmlns="%(namespace)s">
+<SubscriberID>%(subscriberid)s</SubscriberID>
+<Version>8.1.0.1</Version>
+<DossierName>%(dossiername)s</DossierName>
+<Timestamp>%(timestamp)s</Timestamp>
 %(content)s
 </VV_TM_PUSH>""" % data
 
-		return xml
-	
-	def push(self, remote, path):
-		content = str(self)
-		if debug:
-			print content
+        return xml
 
-		if send:
-			conn = HTTPConnection(remote)
-			conn.request("POST", path, content, {"Content-type": "application/xml"})
-			response = conn.getresponse()
-			data = response.read()
-			conn.close()
-			if debug:
-				print data
+    def push(self, remote, path):
+        response = None
+        content = str(self)
+        if debug:
+            print content
 
-			return data
-		
-		return None
+        if send:
+            conn = HTTPConnection(remote)
+            conn.request("POST", path, content, {"Content-type": "application/xml"})
+            response = conn.getresponse().read()
+            conn.close()
+            if debug:
+                print response
+
+        if store and response is not None and 'ResponseCode>OK<' in response:
+            self.content.save()
+
+        return response
