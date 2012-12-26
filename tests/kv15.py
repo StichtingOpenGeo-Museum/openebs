46489a3d0b45c888b90503eed7e95f5382ca512b
Stefan de Konink <stefan@konink.de>
2012-12-26 14:13:16 +0100

    Add DataOwnerCode to the test

--- tests/kv15.py
+++ tests/kv15.py
@@ -30,7 +30,7 @@ YY = (XX + 1) % 24
 # REMOVE
 
 waitforit(datetime.combine(date.today(), time(XX, 00)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='GENERAL REMOVE')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='GENERAL REMOVE')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.REMOVE
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -47,7 +47,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 # ENDTIME
 
 waitforit(datetime.combine(date.today(), time(XX, 15)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='GENERAL ENDTIME')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='GENERAL ENDTIME')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.ENDTIME
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -58,7 +58,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 waitforit(datetime.combine(date.today(), time(XX, 20)))
 
 waitforit(datetime.combine(date.today(), time(XX, 25)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='GENERAL ENDTIME2')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='GENERAL ENDTIME2')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.ENDTIME
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -76,7 +76,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 # FIRSTVEJO
 
 waitforit(datetime.combine(date.today(), time(XX, 35)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='GENERAL FIRSTVEJO')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='GENERAL FIRSTVEJO')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.FIRSTVEJO
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -86,7 +86,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 
 
 waitforit(datetime.combine(date.today(), time(XX, 45)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='GENERAL FIRSTVEJO2')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='GENERAL FIRSTVEJO2')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.FIRSTVEJO
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -103,7 +103,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 # REMOVE
 
 waitforit(datetime.combine(date.today(), time(XX, 50)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='OVERRULE REMOVE')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE REMOVE')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.REMOVE
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -120,7 +120,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 # ENDTIME
 
 waitforit(datetime.combine(date.today(), time(YY, 00)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='OVERRULE ENDTIME')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE ENDTIME')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.ENDTIME
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -131,7 +131,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 waitforit(datetime.combine(date.today(), time(YY, 05)))
 
 waitforit(datetime.combine(date.today(), time(YY, 10)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='OVERRULE ENDTIME2')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE ENDTIME2')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.ENDTIME
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -149,7 +149,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 # FIRSTVEJO
 
 waitforit(datetime.combine(date.today(), time(YY, 20)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='OVERRULE FIRSTVEJO')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE FIRSTVEJO')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.FIRSTVEJO
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -159,7 +159,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 
 
 waitforit(datetime.combine(date.today(), time(YY, 30)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='OVERRULE FIRSTVEJO2')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE FIRSTVEJO2')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.FIRSTVEJO
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -175,19 +175,24 @@ kv15.push(remote, '/TMI_Post/KV15')
 # Overigen
 
 waitforit(datetime.combine(date.today(), time(YY, 35)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='Overigen')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='OVERRULE FIRSTVEJO')
 msg.messagetype = MessageType.BOTTOMLINE
 msg.mesagedurationtype = MessageDurationType.FIRSTVEJO
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
 kv15 = KV15messages(stopmessages = [msg])
 kv15.push(remote, '/TMI_Post/KV15')
 
+# Delete bij gezet omdat anders de andere testen falen
+waitforit(datetime.combine(date.today(), time(YY, 38)))
+delmsg = msg.delete()
+kv15 = KV15messages(stopmessages = [delmsg])
+kv15.push(remote, '/TMI_Post/KV15')
 
 # Lange vrijetekst met een tekst van
 # ongeveer 200 karakters
 
 waitforit(datetime.combine(date.today(), time(YY, 40)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='Deze aanbieder adviseert over en ontwikkelt, beheert en onderhoudt informatiesystemen voor mobiliteit. Als specialist in multi-modaal verkeer en vervoer zet zij haar kennis en ervaring in voor het hele traject van systeemontwikkeling en -onderhoud.')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='Deze aanbieder adviseert over en ontwikkelt, beheert en onderhoudt informatiesystemen voor mobiliteit. Als specialist in multi-modaal verkeer en vervoer zet zij haar kennis en ervaring in voor het hele traject van systeemontwikkeling en -onderhoud.')
 msg.messagetype = MessageType.GENERAL
 msg.mesagedurationtype = MessageDurationType.REMOVE
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -201,7 +206,7 @@ kv15.push(remote, '/TMI_Post/KV15')
 
 
 waitforit(datetime.combine(date.today(), time(YY, 50)))
-msg = StopMessage(userstopcodes=['42028501'], messagecontent='Deze aanbieder adviseert over en ontwikkelt, beheert en onderhoudt informatiesystemen voor mobiliteit. Als specialist in multi-modaal verkeer en vervoer zet zij haar kennis en ervaring in voor het hele traject van systeemontwikkeling en -onderhoud.')
+msg = StopMessage(dataownercode='VTN', userstopcodes=['42028501'], messagecontent='Deze aanbieder adviseert over en ontwikkelt, beheert en onderhoudt informatiesystemen voor mobiliteit. Als specialist in multi-modaal verkeer en vervoer zet zij haar kennis en ervaring in voor het hele traject van systeemontwikkeling en -onderhoud.')
 msg.messagetype = MessageType.OVERRULE
 msg.mesagedurationtype = MessageDurationType.REMOVE
 msg.messagestarttime = datetime.combine(date.today(), time(XX, 05))
@@ -212,10 +217,3 @@ waitforit(datetime.combine(date.today(), time(YY, 55)))
 delmsg = msg.delete()
 kv15 = KV15messages(stopmessages = [delmsg])
 kv15.push(remote, '/TMI_Post/KV15')
-
-
-
-
-#kv15 = KV15messages(stopmessages=[StopMessage(userstopcodes=['0000123', '1234000'], messagecontent='Hoi Joost'), StopMessage(userstopcodes=['1000123', '1234001'], messagecontent='Hoi Thomas')])
-#kv15 = KV15messages(stopmessages=[StopMessage(userstopcodes=['0000123', '1234000'], messagecontent='Hoi Joost'), StopMessage(userstopcodes=['1000123', '1234001'], messagecontent='Hoi Thomas')])
-#kv15.push(remote, '/TMI_Post/KV15')
