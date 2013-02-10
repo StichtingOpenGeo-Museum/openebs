aa4533c3f3ae173ae9d8d2142c6f48698dfefaf7
Skywave <thakoch@gmail.com>
2013-02-10 01:22:13 +0100

    Set DRIS-ACC as remote

--- settings/const.py
+++ settings/const.py
@@ -2,7 +2,7 @@ debug = True
 send = True
 store = True
 #remote = 'drisacc.transmodel.nl'
-remote = 'drisacc.transmodel.nl:1234'
+remote = 'drisacc.transmodel.nl'
 remote_path = '/TMI_Post/KV15'
 kv15_database_connect = "dbname='openebs'"
 kv78_database_connect = "dbname='kv78turbo1'"
