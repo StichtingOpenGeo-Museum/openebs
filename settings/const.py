92eaa1b636d87c0e17cf5f25f2af10a861293cc2
Stefan de Konink <stefan@konink.de>
2013-02-19 01:36:08 +0100

    kv78turbo uri

--- settings/const.py
+++ settings/const.py
@@ -6,3 +6,5 @@ remote = 'drisacc.transmodel.nl'
 remote_path = '/TMI_Post/KV15'
 kv15_database_connect = "dbname='openebs'"
 kv78_database_connect = "dbname='kv78turbo1'"
+kv8zmqpub = "tcp://node03.post.openov.nl:7817"
+
